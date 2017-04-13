29a0db70533581966ef7b9d01d358efaad30a19c
Matt Hayden <matt.hayden@mcmaster.com>
2017-04-13 02:28:47 -0500

    starting on discrete variables

--- Final Project/Code/EDA - Hayden.R
+++ Final Project/Code/EDA - Hayden.R
@@ -11,14 +11,17 @@ forest.var.continuous = c('Elevation','Aspect','Slope', 'HDist.Hydrology', 'VDis
 
 # assign factor/discrete variables
 library(dplyr)
-forest.var.discrete.indices = grep("^Area|^SoilType|CoverType", colnames(forest.orig))
-forest.orig[,forest.var.discrete.indices] = as.factor(unlist(forest.orig[,forest.var.discrete.indices]))
 
-summary(forest.orig)
+#summary(forest.orig)
 
 # for speed, will perform eda on subset until ready to do a full run
 set.seed(33)
 forest = forest.orig[sample(nrow(forest.orig),10000),]
+forest[,'SoilType15'] = NULL
+forest.numeric = forest
+forest.var.discrete.indices = grep("^Area|^SoilType|CoverType", colnames(forest))
+forest[,forest.var.discrete.indices] = as.factor(unlist(forest[,forest.var.discrete.indices]))
+
 
 str(forest.orig)
 colnames(forest)
@@ -28,7 +31,8 @@ table(is.na(forest)) # no missing values
 
 ########## DATA TRANSFORMATIONS and VARIABLE MAINTENANCE #########################
 apply(sapply(forest,as.numeric),2,sum) #soil type 15 doesn't have any observations. so remove
-forest[,'SoilType15'] = NULL
+
+forest.numeric = sapply(forest,as.numeric) 
 
 str(forest)
 
@@ -71,4 +75,62 @@ forest.lda.pred = predict(forest.lda,data=forest.wo.covertype)
 tbl = table(forest.lda.pred$class,forest$CoverType)
 plot(tbl)
 addmargins(tbl)
+
+##### DISCRETE VARIABLES ###################################
+
+library(lattice)
+idx = grep("SoilType|CoverType", colnames(forest.numeric))
+df = forest.numeric[,idx]
+idx.type = grep("CoverType", colnames(df))
+
+soil.sums = apply(df[,-idx.type],2,function(x) {
+  tbl = table(x,df$CoverType)
+  return (apply(tbl,1,sum)[2])
+})
+soil.sums
+
+soil.sums.byclass = apply(df[,-idx.type],2,function(x) {
+  tbl = table(x,df$CoverType)
+  tbl = tbl[seq(2,14,by=2)]
+  return (tbl)
+})
+soil.sums.byclass
+
+soil.ratios = as.data.frame(t(soil.sums.byclass)/soil.sums)
+#soil.ratios.t = as.data.frame(t(soil.ratios))
+#soil.ratios.t$CoverType = c('1','2','3','4','5','6','7')
+barchart(as.matrix(soil.ratios))
+
+
+library(reshape2)
+soil.ratios.formatted = melt(soil.ratios.t, SoilType=seq(1:39))
+#b=melt(b, id.vars=c('SoilType'),var='CoverTypes')
+
+#library(corrplot)
+#corrplot(cor(forest.numeric), 
+#         tl.col = "black", tl.cex = 0.8, tl.srt = 45,
+#         cl.cex = 0.8, pch.cex = 0.8, diag = FALSE,
+#         type="lower")
+
+
+soil.sums2 = apply(df[,-idx.type],2,function(x) {
+  tbl = table(x,df$CoverType)
+  tbl = tbl[seq(2,14,by=2)]
+  return (tbl)
+})
+soil.sums2 = apply(soil.sums2,1,sum)
+soil.sums2
+
+soil.sums.byclass2 = apply(df[,-idx.type],2,function(x) {
+  tbl = table(x,df$CoverType)
+  tbl = tbl[seq(2,14,by=2)]
+  #tbl = t(tbl)
+  return (tbl)
+})
+soil.sums.byclass2
+soil.sums.byclass2$CoverType = c('1','2','3','4','5','6','7')
+
+
+#Questions
+# how often is same soil types together
 
